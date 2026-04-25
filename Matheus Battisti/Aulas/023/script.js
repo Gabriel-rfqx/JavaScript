19d79fea3756f3afe17c27f7136bfde5f88f2f06
Gabriel-rfqx <azevedogabriel680@gmail.com>
2026-04-25 13:49:21 -0300

    Update script.js

--- Matheus Battisti/Aulas/023/script.js
+++ Matheus Battisti/Aulas/023/script.js
@@ -1,1 +1,19 @@
 //DOM
+var lista = ['banana', 'maca', 'limao']
+
+var listaul = document.createElement('ul')
+
+var mani = document.getElementsByTagName('main')
+console.log(mani[0])
+mani[0].appendChild(listaul)
+console.log(mani[0].innerHTML)
+var listaColocada = document.getElementsByTagName('ul')
+console.log(listaColocada)
+console.log(listaColocada[0])
+for(var c = 0; c< lista.length; c++){
+    var liFor = document.createElement("li")
+    var textoLi = document.createTextNode(lista[c])
+    liFor.appendChild(textoLi)
+    listaColocada[0].appendChild(liFor)
+    
+}
